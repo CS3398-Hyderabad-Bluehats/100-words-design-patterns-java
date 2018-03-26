da3aec72e2cca852de1fd88dcc5a82431f589993
RyanHoward84 <35548701+RyanHoward84@users.noreply.github.com>
2018-03-26 17:49:23 -0500

    Update Flyweight.java
    making a change and adding my comment to the flyweight java

--- src/main/java/com/hundredwordsgof/flyweight/Flyweight.java
+++ src/main/java/com/hundredwordsgof/flyweight/Flyweight.java
@@ -4,7 +4,7 @@ package com.hundredwordsgof.flyweight;
  * 
  * Flyweight, interface through flyweight can receive and act on extrinsic
  * state.
- *
+ * Ryan Howard edited this file :D
  */
 public interface Flyweight {
 
